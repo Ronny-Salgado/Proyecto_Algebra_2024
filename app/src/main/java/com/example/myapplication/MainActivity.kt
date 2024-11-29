3dd4b657cc47bf28d6eb809c141ccb16c45ea59e
GutierrezJC <gutierrezcarrillo0794@outlook.es>
2024-11-28 21:23:02 -0600

    prueba 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -27,18 +27,18 @@ class MainActivity : AppCompatActivity() {
         }
 
 //        Botón para operaciones con matrices
-        val btnMatrixOperations = findViewById<Button>(R.id.btnMatrixOperations)
+/*        val btnMatrixOperations = findViewById<Button>(R.id.btnMatrixOperations)
         btnMatrixOperations.setOnClickListener {
             val intent = Intent(this, MatrixOperationsActivity::class.java)
             startActivity(intent)
-        }
+        }*/
 
 //        // Botón para operaciones con vectores
-//        val btnVectorOperations = findViewById<Button>(R.id.btnVectorOperations)
-//        btnVectorOperations.setOnClickListener {
-//            val intent = Intent(this, VectorOperationsActivity::class.java)
-//            startActivity(intent)
-//        }
+        val btnVectorOperations = findViewById<Button>(R.id.btnVectorOperations)
+        btnVectorOperations.setOnClickListener {
+        val intent = Intent(this, VectorsActivity::class.java)
+          startActivity(intent)
+        }
 //
 //        // Botón para operaciones con rectas
 //        val btnLineOperations = findViewById<Button>(R.id.btnLineOperations)
